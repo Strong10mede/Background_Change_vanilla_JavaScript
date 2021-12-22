3fefa93bfb7971a881d37cfaf27bac0823096a5a
Mayur Kumar <kmayur809@gmail.com>
2021-12-22 12:45:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let bg = document.getElementById('bg');
+function randomColor(){
+    return Math.floor(Math.random() * 255);
+}
+
+bg.addEventListener('click',()=>{
+    bg.style.backgroundColor = 'rgba('+randomColor()+','+randomColor()+','+randomColor()+')'
+})
